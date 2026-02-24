2ce25cb3b414ab39930bda9e59175f76844a0215
Перфильева Карина Андреевна 4731204/50001 <karishka.perf1@gmail.com>
2026-02-24 21:10:16 +0300

    Delete projects_2/task_2_0 directory

--- projects_2/task_2_0/task_2_1/task_2-1_2.py
+++
@@ -1,3 +0,0 @@
-python_version = "3.10"
-print(f"Language: Python\n\tVersion: {python_version}")
-
--- projects_2/task_2_0/task_2_2/task_2-2_1.py
+++
@@ -1,4 +0,0 @@
-name_course = "informatica"
-number_task = 2.2
-done_task = 2
-print(f"Название курса: {name_course}\nНомер текущего этапа: {number_task}\nКоличество выполненных задач: {done_task}")
