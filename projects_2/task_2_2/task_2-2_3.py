d6dfc08c18ab0a8d4a6940eb947c736ab731e1d3
Перфильева Карина Андреевна 4731204/50001 <karishka.perf1@gmail.com>
2026-02-24 21:14:00 +0300

    Add files via upload

---
+++ projects_2/task_2_2/task_2-2_3.py
@@ -0,0 +1,4 @@
+name_device = "микроскоп"
+inventory_number = 675246
+status_device = "исправен"
+print(f"Прибор:\t\t\t{name_device}\nИнвентарный номер:\t{inventory_number}\nСостояние:\t\t{status_device}")
