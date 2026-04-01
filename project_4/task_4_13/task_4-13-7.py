fddc46ccba14a23223cff4e0c62a1aa088b85572
Перфильева Карина Андреевна 4731204/50001 <karishka.perf1@gmail.com>
2026-04-01 21:22:33 +0300

    Update task_4-13-7.py

--- project_4/task_4_13/task_4-13-7.py
+++ project_4/task_4_13/task_4-13-7.py
@@ -1,11 +1,9 @@
 array = [64, 34, 25, 12, 22, 11, 90]
 n = len(array)
-
 sum = 0
+
 for i in range(n):
-    for j in range(n):
-        if i == j:
-            sum += array[j]
+    sum += array[i]
 
 zn = sum / n
 print(round(zn, 2))
